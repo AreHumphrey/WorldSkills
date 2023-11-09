82aed4f48629dee9c5507867463b5147ef85b25e
mrart <bykov.ad@students.dvfu.ru>
2023-11-09 20:11:36 +1000

    meowmeow pro sha256 comment dobavil

--- main.sql
+++ main.sql
@@ -2,7 +2,7 @@ Create Table Users
 (
     Id          int primary key,
     Email       text unique not null,
-    Password    text        not null,
+    Password    text        not null, -- In sha256 hash!!!
     FirstName   text        not null,
     LastName    text        not null,
     Role        varchar(1) check (Role in ('A', 'C', 'E', 'O', 'P', 'V')),
@@ -10,3 +10,4 @@ Create Table Users
     DateOfBirth date,
     RegionCode check (1 <= RegionCode <= 100)
 );
+
