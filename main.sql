cd3a76a06eb5086b65fbc17637974107a9d1faca
mrart <bykov.ad@students.dvfu.ru>
2023-11-09 20:06:55 +1000

    db first commit ))) hello world

---
+++ main.sql
@@ -0,0 +1,12 @@
+Create Table Users
+(
+    Id          int primary key,
+    Email       text unique not null,
+    Password    text        not null,
+    FirstName   text        not null,
+    LastName    text        not null,
+    Role        varchar(1) check (Role in ('A', 'C', 'E', 'O', 'P', 'V')),
+    Gender      text check (Gender in ('Male', 'Female')),
+    DateOfBirth date,
+    RegionCode check (1 <= RegionCode <= 100)
+);
