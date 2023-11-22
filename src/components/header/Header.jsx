fcd9dc095e8a67859888a85ac69fc59272877755
Akexandr Zhukov <147894134+ALXSSSDR@users.noreply.github.com>
2023-11-22 21:54:07 +1000

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import Logoimg from './../../img/sq.white.png'
+import './header.css'
+
+function Header () {
+    return <header className="header"> 
+        <div className="container">
+            <div className="header_row">
+                <div className="header_logo">
+                    <img src={Logoimg} alt='Logo'/>
+                </div>
+                <nav className='header_nav'>
+                    <ul>
+                        <li><a href="#promo-content">О WorldSkills</a></li>
+                        <li><a href="#promo-content2">O WorldSkills Russia</a></li>
+                        <li><a href='#promo-content3'>О Краснодарском крае</a></li>
+                        <li><a href='#!'>Войти</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>   
+    </header>
+    
+}
+
+export default Header
