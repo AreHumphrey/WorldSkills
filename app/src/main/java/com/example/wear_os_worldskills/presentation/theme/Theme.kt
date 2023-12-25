d90d74002a1ebf85c5bd9a002e39ce4829df9096
Dina <lutdina04@yandex.ru>
2023-12-25 19:59:17 +1000

    create

---
+++ app/src/main/java/com/example/wear_os_worldskills/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.wear_os_worldskills.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun Wear_OS_WorldSkillsTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = wearColorPalette,
+        typography = Typography,
+        // For shapes, we generally recommend using the default Material Wear shapes which are
+        // optimized for round and non-round devices.
+        content = content
+    )
+}
